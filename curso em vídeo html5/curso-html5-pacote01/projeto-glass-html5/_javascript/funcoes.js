5608b4c15f1cec0dfb2b31b1444023e16e0dd004
m1mir <91437685+m1mir-the-wise@users.noreply.github.com>
2022-07-26 22:40:26 -0300

    Site Projeto Glass
    Site do Google Glass, do curso de HTML5 e CSS3 do canal Curso em Vídeo

---
+++ curso em vídeo html5/curso-html5-pacote01/projeto-glass-html5/_javascript/funcoes.js
@@ -0,0 +1,8 @@
+function mudaFoto (foto) {
+    document.getElementById("icone").src = foto;
+}
+function calcTotal() {
+    var qtd = parseInt(document.getElementById('cQtd').value);
+    tot = qtd * 1500;
+    document.getElementById('cTot').value = tot;
+}
